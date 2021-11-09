0244a4b7e990a0c8929eea0b565b7da0cc76325e
LevonSirunyan <levonsirunyan@gmail.com>
2021-11-09 21:16:28 +0400

    create new project, first commit

---
+++ src/main/java/am/springbatch/CsvWithSpringBatch.java
@@ -0,0 +1,11 @@
+package am.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvWithSpringBatch {
+    public static void main(String[] args) {
+        SpringApplication.run(CsvWithSpringBatch.class, args);
+    }
+}
